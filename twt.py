f636c76cff0ffcfa7cd8e1b5b248ffd866592b88
joxar <totechite@gmail.com>
2018-01-13 19:20:31 +0000

    utf-8

--- twt.py
+++ twt.py
@@ -1,7 +1,8 @@
+# encoding: utf-8
 import requests
 
 TOKEN = ""
 URL = "https://maker.ifttt.com/trigger/voiceTweet/with/key/"
 
-requests.post(URL + TOKEN, json = {'value1':'abc'})
+requests.post(URL + TOKEN, json = {'value1':'ためしにツイート'})
 
